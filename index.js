0646df65145646e5c5f53941f8dbf122460a0520
ismaicetin <ismaicetin@hotmail.com>
2021-03-22 17:52:31 +0300

    ade

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json({ limit: "100mb" }));
 app.use(cors());
 const ccxt = require("ccxt");
 var WebSocketServer = require("ws").Server,
-	wss = new WebSocketServer({ port: 3001 });
+	wss = new WebSocketServer({ port: 8080 });
 
 // setInterval(() => wsRun(), 1000);
 
